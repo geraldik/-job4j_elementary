285c4f55aeae8d2fa66ab4b036b3c9c5d0ee6b5f
geraldik <geraldik1986@gmail.com>
2021-12-14 13:39:40 +0300

    Добавлен вызов метода с отрицательным аргументом.

--- src/main/java/ru/job4j/condition/AlertDivByZero.java
+++ src/main/java/ru/job4j/condition/AlertDivByZero.java
@@ -4,7 +4,7 @@ public class AlertDivByZero {
     public static void main(String[] args) {
         possibleDiv(4);
         possibleDiv(0);
-        possibleDiv(-5);
+        possibleDiv(-4);
     }
 
     public static void possibleDiv(int number) {
