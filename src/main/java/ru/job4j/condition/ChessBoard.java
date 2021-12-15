f77ae72b0bb956f896979dc3d2d7b576111369cd
geraldik <geraldik1986@gmail.com>
2021-12-15 20:48:10 +0300

    Убрана лишняя переменная.

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -4,9 +4,8 @@ public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
         int rsl = 0;
         if (x1 >= 0 && x1 <= 7 && y1 >= 0 && y1 <= 7 && x2 >= 0 && x2 <= 7 && y2 >= 0 && y2 <= 7) {
-            int delta1 = Math.abs(x2 - x1);
-            int delta2 = Math.abs(y2 - y1);
-            rsl = (delta1 == delta2) ? delta1 : 0;
+            int delta = Math.abs(x2 - x1);
+            rsl = (delta == Math.abs(y2 - y1)) ? delta : 0;
         }
         return rsl;
     }
