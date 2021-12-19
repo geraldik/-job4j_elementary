77a5f758ab9524f8cf724e793c3b7d63ba080b2a
geraldik <geraldik1986@gmail.com>
2021-12-19 15:33:27 +0300

    Исправлено условие цикла. Убран условный оператор.

--- src/main/java/ru/job4j/array/Turn.java
+++ src/main/java/ru/job4j/array/Turn.java
@@ -2,11 +2,8 @@ package ru.job4j.array;
 
 public class Turn {
     public static int[] back(int[] array) {
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length / 2; i++) {
             int temp = array[i];
-            if (i >= array.length - i - 1) {
-                break;
-            }
             array[i] = array[array.length - i - 1];
             array[array.length - i - 1] = temp;
         }
