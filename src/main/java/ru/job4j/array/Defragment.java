90324b7c49378aec3a1f5d5d2b9f6e3d8ddf08e6
geraldik <geraldik1986@gmail.com>
2021-12-21 13:57:37 +0300

    Переписано тело метода compress.

--- src/main/java/ru/job4j/array/Defragment.java
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -2,25 +2,16 @@ package ru.job4j.array;
 
 public class Defragment {
     public static String[] compress(String[] array) {
-        int nullindex = 0;
-        int notNullIndex = 0;
         for (int i = 0; i < array.length; i++) {
-            for (int j = i; j < array.length; j++) {
-                if (array[j] == null) {
-                    nullindex = j;
-                    break;
+            if (array[i] == null) {
+                for (int j = i + 1; j < array.length; j++) {
+                    if (array[j] != null) {
+                        array[i] = array[j];
+                        array[j] = null;
+                        break;
+                    }
                 }
             }
-            for (int j = i; j < array.length; j++) {
-                if (array[j] != null) {
-                    notNullIndex = j;
-                    break;
-                }
-            }
-        if (notNullIndex > nullindex) {
-            array[nullindex] = array[notNullIndex];
-            array[notNullIndex] = null;
-        }
         }
         return array;
     }
